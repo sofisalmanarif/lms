b845cb73968ccf93bfca0e8ea79827cdf73f3fdc
sofisalmanarif <sofisalman9906@gmail.com>
2024-12-12 21:01:19 +0530

    ui changes in book card

--- client/src/components/ui/BookCard.tsx
+++ client/src/components/ui/BookCard.tsx
@@ -3,27 +3,27 @@ import { Button } from "@/components/ui/button"
 import {
   Card,
   CardContent,
-  CardFooter,
-  CardHeader,
 } from "@/components/ui/card"
 
 
 const BookCard = () => {
     return (
-        <Card className="sm:w-[300px] w-[170px] p-2">
-          <CardHeader className="sm:py-4 p-1">
-          </CardHeader>
-            <img src="/book.png" className="sm:h-32 sm:w-full h-20 w-full" alt="" />
-          <CardContent className="flex flex-col px-2 sm:py-2 py-1 my-2" >
-            <h4 className="text-black px-2 text-center sm:text-sm text-xs flex items-center justify-center bg-yellow-200/50 w-auto mx-auto rounded-md mb-2 ">Programing</h4>
-            <div className="text-xs sm:text-sm flex flex-col sm:my-2 ">
+        <Card className="sm:w-[300px] min-w-[170px] p-2">
+          <div className="sm:h-40 h-24  flex items-center justify-between">
+
+            <img src="/book.png" className="sm:h-36 sm:w-full h-20 w-full" alt="" />
+          </div>
+          <CardContent className="flex flex-col px-1 sm:py-2 py-1 my-2" >
+            <p className="text-green-500 px-1 text-center sm:text-sm text-[8px] block items-center justify-center bg-green-200/25 border-2 border-green-400 w-auto mx-auto rounded-md mb-2  ">Programing</p>
+            <div className="text-xs text-gray-600 sm:text-sm flex flex-col sm:my-2 ">
             <span>Name: You Don't know js</span>
             <span>Author:Kyle Simpson</span>
+            {/* <span>Edition:Kyle Simpson</span> */}
             </div>
           </CardContent>
-          <CardFooter className="w-full mx-0 sm:p-2 pb-1 ">
-            <Button  className="w-full">Reserve Now</Button>
-          </CardFooter>
+          <div className="w-full mx-0 sm:p-2 pb-1 ">
+            <Button  variant={"default"} className="w-full font-semibold">Reserve Book</Button>
+          </div>
         </Card>
         )
 }
