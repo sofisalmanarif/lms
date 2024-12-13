5edf2dac417c340b8db85154bb914c186ed9ef89
sofisalmanarif <sofisalman9906@gmail.com>
2024-12-13 14:17:13 +0530

    few ui change s in book details page

--- client/src/pages/BookDetails.tsx
+++ client/src/pages/BookDetails.tsx
@@ -14,7 +14,8 @@ const BookDetails = () => {
             <div className='sm:pr-20 pr-5 border-b-[1px] border-gray-300 sm:pb-10 pb-4 py-2 sm:mb-10 mb-6' >
                 <div className='absolute top-0 right-4'><Button className='font-semibold'><LocateIcon size={150}/></Button></div>
                 
-                <p className='sm:text-4xl  text-2xl  font-semibold'>You Don't know js</p>
+                <h1 className='sm:text-4xl  text-2xl  font-semibold'>You Don't know js</h1>
+                <div className="text-green-500 px-1 text-center  sm:text-sm text-[10px]  items-center justify-center bg-green-200/25 border-2 border-green-300  w-fit rounded-md mb-1  sm:mb-2 ">Programing</div>    
                 <div className='sm:mb-4 mb-2 text-gray-600  sm:text-md text-sm'>
                 <h6 className='text-lg'>Kyle Simpson</h6>
                 
@@ -23,7 +24,6 @@ const BookDetails = () => {
                 <h6 className=''>Book Description : Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi laborum cupiditate fuga unde voluptatum, quibusdam aliquam facilis. Quaerat consequatur molestias voluptatibus debitis delectus adipisci a obcaecati, cum tempora eaque minus, voluptatem provident explicabo at!</h6>
                 </div>
 
-                <div className="text-green-500 px-1 text-center  sm:text-sm text-[8px]  items-center justify-center bg-green-200/25 border-2 border-green-300  w-fit rounded-md  sm:my-4 ">Programing</div>    
             </div>
             <Button>Reserve Book </Button>
 
