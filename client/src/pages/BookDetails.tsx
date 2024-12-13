d64e3f2631eb9430083ab950d1980d26f632e557
sofisalmanarif <sofisalman9906@gmail.com>
2024-12-13 14:21:37 +0530

    location button position changed

--- client/src/pages/BookDetails.tsx
+++ client/src/pages/BookDetails.tsx
@@ -5,14 +5,14 @@ import { LocateIcon } from 'lucide-react'
 const BookDetails = () => {
   return (
     <main className='container mx-auto'>
-        <div className='flex items-start justify-start flex-col sm:flex-row sm:p-8 p-2 sm:border-[1px] border-0 border-gray-200 rounded-md
+        <div className='flex relative items-start justify-start flex-col sm:flex-row sm:p-8 p-2 sm:border-[1px] border-0 border-gray-200 rounded-md
         gap-4'>
         <div className='sm:w-[40%] sm:h-[40vh] w-full h-[30vh] border-[1px] border-gray-200 rounded-md flex  items-center justify-center'> 
             <img src="/book.png" alt="" />
         </div>
-        <div className='sm:w-[60%] w-full relative'>
+        <div className='sm:w-[60%] w-full '>
             <div className='sm:pr-20 pr-5 border-b-[1px] border-gray-300 sm:pb-10 pb-4 py-2 sm:mb-10 mb-6' >
-                <div className='absolute top-0 right-4'><Button className='font-semibold'><LocateIcon size={150}/></Button></div>
+                <div className='absolute top-4 right-4'><Button className='font-semibold'><LocateIcon size={150}/></Button></div>
                 
                 <h1 className='sm:text-4xl  text-2xl  font-semibold'>You Don't know js</h1>
                 <div className="text-green-500 px-1 text-center  sm:text-sm text-[10px]  items-center justify-center bg-green-200/25 border-2 border-green-300  w-fit rounded-md mb-1  sm:mb-2 ">Programing</div>    
@@ -25,7 +25,7 @@ const BookDetails = () => {
                 </div>
 
             </div>
-            <Button>Reserve Book </Button>
+            <Button className='font-semibold'>Reserve Book </Button>
 
         </div>
 
