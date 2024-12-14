04acdf01945d8949ad9f020f7a16c352ab298747
sofisalmanarif <sofisalman9906@gmail.com>
2024-12-14 23:07:22 +0530

    home page done

--- client/src/components/BookCard.tsx
+++ client/src/components/BookCard.tsx
@@ -9,13 +9,13 @@ import { Link } from "react-router-dom"
 
 const BookCard = () => {
     return (
-        <Card className="sm:w-[300px] sm:max-w-[300px] min-w-[170px] max-w-[170px] place-items-stretch mb-4 p-2 sm:pb-4 ">
-          <Link to={"/book/1"} className="sm:h-44 h-24 w-full  flex items-center justify-between">
+        <Card className="md:w-[16vw] md:max-w-[16vw] min-w-[170px] max-w-[45vw]   md:max-h-[40vh] place-items-stretch mb-4 p-2 sm:pb-4 ">
+          <Link to={"/book/1"} className="md:h-[10vw] h-24 w-full bg-gray-300/50  rounded-md flex items-center justify-between">
 
-            <img src="/book.png" className="sm:h-36 sm:w-full h-20 w-full" alt="" />
+            <img src="/book.png" className="md:h-[8vw] md:w-full h-20 w-full" alt="" />
           </Link>
           <CardContent className="flex flex-col px-1 sm:py-2 py-1 my-2" >
-            <p className="text-green-500 px-1 text-center sm:text-sm text-[8px] block items-center justify-center bg-green-200/25 border-2  border-green-300 w-auto mx-auto rounded-md mb-2  ">Programing</p>
+            <p className="text-green-500 px-1 text-center sm:text-sm text-[8px] block items-center justify-center bg-green-200/25 border-[1px]  border-green-300 w-auto mx-auto rounded-md mb-2  ">Programing</p>
             <div className="text-xs text-gray-600 sm:text-sm flex flex-col sm:my-2 ">
             <span>Name: You Don't know js</span>
             <span>Author:Kyle Simpson</span>
