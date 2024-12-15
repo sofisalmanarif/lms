b1dfaf9b1e8d447025d7e82308b380d13a21f0b3
sofisalmanarif <sofisalman9906@gmail.com>
2024-12-15 18:28:55 +0530

    bg-removed of new req page

--- client/src/pages/Superadmin/NewLibraryRequset.tsx
+++ client/src/pages/Superadmin/NewLibraryRequset.tsx
@@ -6,7 +6,7 @@ import { LinraryRequestTable } from "@/components/Dashboard/LibraryRequestTable"
 export function NewLibraryRequest() {
   return (
    
-        <div className="flex flex-1 z-10 flex-col bg-red-200 max-h-[93vh]  sm:gap-4 sm:px-20 gap-0 p-4 ">
+        <div className="flex flex-1 z-10 flex-col  max-h-[93vh]  sm:gap-4 sm:px-20 gap-0 p-4 ">
           <h1 className="sm:text-4xl text-2xl text-orange-500 sm:mt-10 mt-0">New Requests</h1>
           <LinraryRequestTable/>
         </div>
