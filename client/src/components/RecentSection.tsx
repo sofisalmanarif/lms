ac514e4cfc7f8ce7097173f274bad9fa67c2c138
sofisalmanarif <sofisalman9906@gmail.com>
2024-12-15 19:27:15 +0530

    book card adjusted

--- client/src/components/RecentSection.tsx
+++ client/src/components/RecentSection.tsx
@@ -5,7 +5,7 @@ const RecentSection = () => {
   return (
     <section className="flex flex-col mx-auto sm:my-20 my-4 sm:mt-20 w-[95%] sm:max-w-[1780px] sm:gap-4 gap-2 ">
         <h2 className="text-center sm:text-4xl text-xl font-semibold ">Recently Added Books</h2>
-        <div className="flex md:max-h-[50vh] justify-between overflow-x-scroll sm:gap-6 gap-4">
+        <div className="flex md:max-h-[60vh] justify-between overflow-x-scroll sm:gap-6 gap-4">
         <BookCard/>
         <BookCard/>
         <BookCard/><BookCard/><BookCard/>
