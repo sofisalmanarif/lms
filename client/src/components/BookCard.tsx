d1af7308db7b511f880bf0d976648a8925332cfb
sofisalmanarif <sofisalman9906@gmail.com>
2024-12-25 17:58:43 +0530

    book card done

--- client/src/components/BookCard.tsx
+++ client/src/components/BookCard.tsx
@@ -1,9 +1,5 @@
  
 import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-} from "@/components/ui/card"
 import { Link } from "react-router-dom"
 import { TypographyP } from "./Typogrphies"
 import { Book } from "@/data/data"
@@ -11,29 +7,30 @@ import { Book } from "@/data/data"
 
 const BookCard = ({book}:{book?:Book}) => {
     return (
-        <Card className="md:w-[16vw] md:max-w-[18vw] min-w-[170px]  max-w-[45vw]    md:max-h-[60vh] place-items-stretch mb-4 p-2 sm:pb-4 ">
-          <Link to={`/book/${book?.id}`} className="md:h-[10vw] h-24 w-full bg-gray-300/50  rounded-md flex items-center justify-between">
+        <div className="md:w-[16vw] border-[1px] border-gray-200  rounded-lg md:max-w-[18vw] min-w-[45.2vw] md:min-w-[17vw]  max-w-[47vw]  md:h-[40vh] min-h-[31vh]   md:max-h-[60vh] place-items-stretch mb-0 p-2 sm:pb-4 ">
+          <Link to={`/book/${book?.id}`} className="md:h-[10vw] h-[45%] w-full bg-gray-300/50  rounded-md flex items-center justify-between">
 
-            <img src={book?.image} className="md:h-[8vw]  mx-auto h-20 " alt="" />
+            <img src={book?.image} className="md:h-[8vw]  mx-auto h-24 " alt="" />
           </Link>
-          <CardContent className="flex flex-col px-1 sm:py-2 py-1 my-2" >
-            <p className="text-green-500 px-1 text-center sm:text-sm text-[8px] block items-center justify-center bg-green-200/25 border-[1px]  border-green-300 w-auto mx-auto rounded-md mb-2  ">{book?.genre}</p>
+          <div className="flex flex-col justify-evenly  md:h-[35%] h-[40%] px-1 sm:py-2 py-1 my-t" >
+            <p className="text-green-500 px-1 text-center sm:text-sm text-[8px] block items-center justify-center bg-green-200/25 border-[1px]  border-green-300 w-auto mx-auto rounded-md mb-1  ">{book?.genre}</p>
             <div className="text-xs text-gray-600 sm:text-sm flex flex-col sm:my-2 ">
   
-            <TypographyP className="leading-none">
+            <p className="mb-2">
             Name: {book?.title}
-              </TypographyP>            
+              </p>            
               
-              <TypographyP className="leading-3">
+              <p className="mb-2">
               Author: {book?.author}
-              </TypographyP>
+              </p>
             {/* <span>Edition:Kyle Simpson</span> */}
             </div>
-          </CardContent>
-          <div className="w-full mx-0 sm:p-2 pb-1 ">
-            <Button  variant={"default"} size={"sm"} className="w-full font-semibold">Reserve Book</Button>
           </div>
-        </Card>
+          <div className="w-full mx-0 sm:p-0 mt-1   bg-red-200 ">
+            
+            <Button  variant={"default"} size={"sm"} className="w-full font-semibold mb-0">Reserve Book</Button>
+          </div>
+        </div>
         )
 }
 
