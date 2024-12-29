962a2aca8c04b7669148e4bd6beec4a2ba29e0a8
sofisalmanarif <sofisalman9906@gmail.com>
2024-12-29 15:57:29 +0530

    logger added

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,32 @@
 import express from "express"
+import logger from "./logger.js";
+import morgan from "morgan";
 
-const app  = express()
 
-app.get("/",(req,res)=>{
+const app = express()
+
+
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+    morgan(morganFormat, {
+        stream: {
+            write: (message: string) => {
+                const logObject = {
+                    method: message.split(" ")[0],
+                    url: message.split(" ")[1],
+                    status: message.split(" ")[2],
+                    responseTime: message.split(" ")[3],
+                };
+                logger.info(JSON.stringify(logObject));
+            },
+        },
+    })
+);
+
+app.get("/", (req, res) => {
     res.send("hello world")
 })
 
 
-export {app}
+export { app }
